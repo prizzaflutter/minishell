b70c622c9c703fae074cbd8be4128179f2406ee9
Ayoub Kassimi <aykassim@e3r4p14.1337.ma>
2025-04-09 11:28:33 +0100

    zaki info

--- lib/ft_lstadd_back.c
+++ lib/ft_lstadd_back.c
@@ -1,25 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aykassim <aykassim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/09 14:36:25 by aykassim          #+#    #+#             */
-/*   Updated: 2025/04/09 11:01:34 by aykassim         ###   ########.fr       */
+/*   Created: 2025/04/09 11:19:20 by aykassim          #+#    #+#             */
+/*   Updated: 2025/04/09 11:19:21 by aykassim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_token	*ft_lstlast(t_list *lst)
+void	ft_lstadd_back(t_token **lst, t_token *new)
 {
-	t_list	*current;
+	t_token	*last;
 
-	if (!lst)
-		return (NULL);
-	current = lst;
-	while (current -> next != NULL)
-		current = current -> next;
-	return (current);
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
 }
--- lib/ft_lstnew.c
+++ lib/ft_lstnew.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aykassim <aykassim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/09 10:57:34 by aykassim          #+#    #+#             */
-/*   Updated: 2025/04/09 10:57:47 by aykassim         ###   ########.fr       */
+/*   Created: 2025/04/09 11:19:06 by aykassim          #+#    #+#             */
+/*   Updated: 2025/04/09 11:19:09 by aykassim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+t_token	*ft_lstnew(char *content)
 {
-	t_list	*last;
+	t_token	*nvtlist;
 
-	if (!lst || !new)
-		return ;
-	if (!*lst)
-		*lst = new;
-	else
-	{
-		last = ft_lstlast(*lst);
-		last->next = new;
-	}
+	nvtlist = (t_token *) malloc(sizeof(t_token));
+	if (!nvtlist)
+		return (NULL);
+	nvtlist->str = content;
+	nvtlist->type = 0;
+	nvtlist->next = NULL;
+	return (nvtlist);
 }
