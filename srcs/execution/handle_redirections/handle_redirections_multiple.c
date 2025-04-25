9fced87b3d044bb5d1ab45e301f2412c3ea99d34
Idriss Askour <iaskour@e1r5p2.1337.ma>
2025-04-25 11:22:05 +0100

    handle multiple redirections with multiple pipes

--- srcs/execution/handle_redirections/handle_redirections_multiple.c
+++ srcs/execution/handle_redirections/handle_redirections_multiple.c
@@ -1,46 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_redirections_multiple.c                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iaskour <iaskour@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 11:54:25 by iaskour           #+#    #+#             */
+/*   Updated: 2025/04/25 10:59:06 by iaskour          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int handle_redirections_multiple(t_command *current_cmd, int fd_array[])
+// echo "hello world" >> aa.txt >> bb.txt >>  cc.txt | echo "world hello" >> 1.txt >> 2.txt >> 3.txt
+
+int handle_redirections_multiple(t_command *cmd, int fd_array[])
 {
     int out_file;
+	int	in_file;
+	int	i;
+	(void)fd_array;
 
     out_file = -1;
-    if ((current_cmd->inoutfile && current_cmd->inoutfile[0] && !ft_strncmp(current_cmd->inoutfile[0], ">", 1)) || 
-			(current_cmd->inoutfile && current_cmd->inoutfile[0] && !ft_strncmp(current_cmd->inoutfile[0], ">>", 2)))
+	i = 0;
+	while (cmd->inoutfile && cmd->inoutfile[i])
+	{
+		if (!ft_strcmp(cmd->inoutfile[i], ">>"))
 		{
-			ft_printf(2, "the command contains redirection\n");
-			if (!ft_strncmp(current_cmd->inoutfile[0], ">>", 2))
-				out_file = open(current_cmd->inoutfile[1], O_WRONLY | O_CREAT | O_APPEND , 0642);
-			else if(!ft_strncmp(current_cmd->inoutfile[0], ">", 1))
-				out_file = open(current_cmd->inoutfile[1], O_WRONLY | O_CREAT | O_TRUNC, 0642);
-			if (out_file == -1)
+			if (out_file != -1)
+				close(out_file);
+			out_file = open (cmd->inoutfile[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+			if ( out_file == -1)
+				return (-1);
+		}
+		else if (!ft_strcmp(cmd->inoutfile[i], ">"))
+		{
+			if (out_file != -1)
+				close(out_file);
+			out_file = open(cmd->inoutfile[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if(out_file)
+				return (-1);
+		}
+		else if (!ft_strcmp(cmd->inoutfile[i], "<"))
+		{
+			in_file = open(cmd->inoutfile[i + 1], O_RDONLY);
+			if (in_file == -1)
 			{
-				ft_printf(2, "error in file descriptor 1\n");
+				printf("error in file descriptor single command 2\n");
 				return (-1);
 			}
-			dup2(out_file, STDOUT_FILENO);
-			close(out_file);
+			dup2(in_file, STDIN_FILENO);
+			close(in_file);
 		}
-		// else if (current_cmd->inoutfile && current_cmd->inoutfile[0] && !ft_strncmp(current_cmd->inoutfile[0], "<<", 2))
-		// {
-		// 	handle_herdoc_infile(current_cmd);
-		// }ll
-		else if (current_cmd->inoutfile && current_cmd->inoutfile[0] && !ft_strncmp(current_cmd->inoutfile[0], "<", 1))
-		{
-			out_file = open(current_cmd->inoutfile[1], O_RDONLY);
-			if (out_file == -1)
-			{
-				ft_printf(2, "error in file descriptor 2\n");
-				return (-1);
-			}
-			dup2(out_file, STDIN_FILENO);
-			close(out_file);
-		}
-		else if (current_cmd->next)
-		{
-			close(fd_array[1]);
-			dup2(fd_array[0], STDIN_FILENO);
-			close(fd_array[0]);
-		}
-    return (out_file);
+		i += 2;
+	}
+	if (cmd->next && out_file == -1)
+	{
+		close(fd_array[0]);
+		dup2(fd_array[1], STDOUT_FILENO);
+		close(fd_array[1]);
+	}
+	if (out_file != -1)
+	{
+		dup2(out_file, STDOUT_FILENO);
+		close(out_file);
+	}
+    return (out_file != 1);
 }
