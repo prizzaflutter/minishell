2d8ba0ad2995bb013d6c645c2040601bfa492988
Ayoub Kassimi <aykassim@e2r5p1.1337.ma>
2025-05-19 12:51:07 +0100

    handle askour

--- srcs/execution/builtin/cd/go_up.c
+++ srcs/execution/builtin/cd/go_up.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   go_up.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iaskour <iaskour@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aykassim <aykassim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:16:02 by iaskour           #+#    #+#             */
-/*   Updated: 2025/05/19 10:58:02 by iaskour          ###   ########.fr       */
+/*   Updated: 2025/05/19 12:50:08 by aykassim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// just ignore the node skip it 
 
 void	go_up(t_stack **stack)
 {
@@ -31,6 +33,7 @@ void	go_up(t_stack **stack)
 		current = current->next;
 	prev = current->next;
 	free(prev->path);
+	
 	free(prev);
 	current->next = NULL;
 }
--- srcs/execution/builtin/pwd.c
+++ srcs/execution/builtin/pwd.c
@@ -1,22 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: iaskour <iaskour@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/20 12:43:26 by iaskour           #+#    #+#             */
-/*   Updated: 2025/05/19 10:50:27 by iaskour          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
 void	my_pwd(t_env *env)
 {
 	char	content[4096];
 	char	*cwd;
-	
+
 	cwd = getcwd(content, sizeof(content));
 	if (cwd)
 		printf("%s\n", content);
