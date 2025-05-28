d5ffd4ca69b348ac8d477175128e1ae61e793244
Ayoub Kassimi <aykassim@e2r5p1.1337.ma>
2025-05-28 15:24:50 +0100

    handle cases of expand

--- srcs/parsing/handle_input.c
+++ srcs/parsing/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: aykassim <aykassim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 13:14:22 by aykassim          #+#    #+#             */
-/*   Updated: 2025/05/28 09:57:38 by aykassim         ###   ########.fr       */
+/*   Updated: 2025/05/28 15:13:35 by aykassim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	handle_expand_dollar_sign(t_gc *gc, t_token **tokens,
 
 	i = 0;
 	new_str = handle_expand(gc, str, env);
-	if (detect_quotes(new_str) == 1)
+	if (detect_dollar_sign_insquote(str) == 1)
 	{
 		new_str = handle_double_single_quotes(gc, new_str);
 		add_element_to_tokens(gc, tokens, new_str);
@@ -29,6 +29,32 @@ void	handle_expand_dollar_sign(t_gc *gc, t_token **tokens,
 	else
 	{
 		new_str = handle_double_single_quotes(gc, new_str);
+		char_tmp = ft_split(gc, new_str);
+		i = 0;
+		while (char_tmp[i])
+		{
+			add_element_to_tokens(gc, tokens, char_tmp[i]);
+			i++;
+		}
+	}
+}
+
+void	handle_expand_dollar_sign_echo(t_gc *gc, t_token **tokens,
+	t_env *env, char *str)
+{
+	char	*new_str;
+	char	**char_tmp;
+	int		i;
+
+	i = 0;
+	new_str = handle_expand(gc, str, env);
+	if (detect_quotes(str) == 1)
+	{
+		new_str = handle_double_single_quotes(gc, new_str);
+		add_element_to_tokens(gc, tokens, new_str);
+	}
+	else
+	{
 		char_tmp = ft_split(gc, new_str);
 		i = 0;
 		while (char_tmp[i])
@@ -85,6 +111,33 @@ void	split_when_dollarsign_bf_exp(t_gc *gc, t_token **tokens,
 	}
 }
 
+// void	all_the_work(t_gc *gc, t_token **tokens, t_env *env, t_token *tmp)
+// {
+// 	t_str_inputs	*instr;
+
+// 	instr = gc_malloc(gc, sizeof(t_str_inputs), 0);
+// 	if ((tmp->prev && tmp->prev->prev
+// 		&& (ft_strcmp(tmp->prev->str, "export") == 0)
+// 		&& its_have_dollar_signe(tmp->str)
+// 		&& its_have_dollar_signe(tmp->prev->prev->str)) || its_have_dollar_signe(tmp->str))
+// 		split_when_dollarsign_bf_exp(gc, tokens, tmp->str, env);
+// 	else if (tmp->prev && (ft_strcmp(tmp->prev->str, "export") == 0)
+// 		&& its_have_dollar_signe(tmp->str))
+// 	{
+// 		instr->export = gc_strdup(gc, tmp->prev->str);
+// 		instr->str = gc_strdup(gc, tmp->str);
+// 		handle_expand_dollar_sign_export(gc, tokens, env, instr);
+// 	}
+// 	else if (tmp->prev && ft_strcmp(tmp->prev->str, "echo") == 0)
+// 		add_element_to_tokens(gc, tokens, tmp->str);
+// 	else if (tmp->prev && (tmp->prev->type == REDIR_IN
+// 			|| tmp->prev->type == REDIR_OUT || tmp->prev->type == APPEND))
+// 		handle_val_before_addtokens(gc, tokens, tmp->str);
+// 	else
+// 		handle_val_before_addtokens(gc, tokens, tmp->str);
+// }
+
+
 void	all_the_work(t_gc *gc, t_token **tokens, t_env *env, t_token *tmp)
 {
 	t_str_inputs	*instr;
@@ -102,6 +155,9 @@ void	all_the_work(t_gc *gc, t_token **tokens, t_env *env, t_token *tmp)
 		instr->str = gc_strdup(gc, tmp->str);
 		handle_expand_dollar_sign_export(gc, tokens, env, instr);
 	}
+	else if (tmp->prev && (ft_strcmp(tmp->prev->str, "echo") == 0)
+		&& its_have_dollar_signe(tmp->str))
+		handle_expand_dollar_sign_echo(gc, tokens, env, tmp->str);
 	else if (its_have_dollar_signe(tmp->str))
 		handle_expand_dollar_sign(gc, tokens, env, tmp->str);
 	else if (tmp->prev && ft_strcmp(tmp->prev->str, "echo") == 0)
@@ -112,6 +168,7 @@ void	all_the_work(t_gc *gc, t_token **tokens, t_env *env, t_token *tmp)
 	else
 		handle_val_before_addtokens(gc, tokens, tmp->str);
 }
+
 
 int	add_command_element(t_gc *gc, char *str, t_token **tokens, t_env *env)
 {
--- srcs/parsing/main.c
+++ srcs/parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: aykassim <aykassim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 10:06:47 by aykassim          #+#    #+#             */
-/*   Updated: 2025/05/28 09:38:25 by aykassim         ###   ########.fr       */
+/*   Updated: 2025/05/28 11:34:16 by aykassim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	build_execute_cmds_list(t_gc *gc, t_token *tokens,
 	t_command *cmds, t_env **ens)
 {
 	build_command_list(gc, tokens, &cmds);
+	print_command_list(cmds);
 	execute_command(gc, cmds, ens);
 	clean_fd_herdoc(tokens);
 }
